aa8e56762b5b6cec9b9111a2e15d83bf3ceecabf
Kagapay <kagapj@gmail.com>
2025-06-27 18:10:38 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class KagapayApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kagapay',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: HomePage(),
     );
   }
@@ -17,7 +18,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Kagapay')),
-      body: Center(child: Text('Welcome to Kagapay')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to Kagapay', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text('Send Money'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('View Receipts'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
